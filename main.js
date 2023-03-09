c36473948055927da629736a6d578cb84d9af966
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-09 12:54:11 -0500

    feat: :art: Ejercicio 1
    Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"

--- main.js
+++ main.js
@@ -8,18 +8,16 @@ let notas=[];
 
 function pedirnotas(notas){
     for(let i=0;i<3;i++){
-        let nota = prompt(`Ingrese la nota numero ${i} del estudiante: `);
+        let nota = Number(prompt(`Ingrese la nota numero ${i+1} del estudiante (0.0 a 5.0): `));
         notas.push(nota);
     }
     return notas
 }
+pedirnotas(notas)
+console.log(notas) 
+
+const inicio=0;
+const suma = notas.reduce((acumulado , cur) =>acumulado + cur ,inicio);
+let ifsimp = (suma/3) < 3.9  ? console.log("Estudie bajo, no sea como los de sputnik") : console.log("Sos un pro master estas becado");
 
 
-let suma = notas.reduce ((acumulado , cur) => {
-    return acumulado+cur.notas 
-},0)
-
-suma < 3.9  ? "Estudie no sea como los de sputnik" : "Sos un pro master estas becado"
-
-
-console.log(pedirnotas(notas)) 
