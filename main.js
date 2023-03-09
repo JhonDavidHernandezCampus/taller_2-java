0a35d2a4e3c757c5d8eecc7246ba5093092fd712
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-09 13:11:44 -0500

    fix: :fire: Arreglo ejercicio 2
    2. Dado un número indicar si es par o impar y si es mayor de 10.

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-/* 2. Dado un número indicar si es par o impar y si es mayor de 10.
- 
-*/
+/* 2. Dado un número indicar si es par o impar y si es mayor de 10. */
 
 let numero = Number(prompt(`Ingrese un numero `));
 
-console.log(numero) 
 let paoin= numero%2
-const inicio=0;
 
-paoin=0 ? console.log("El numero es par") : console.log("El numero es impar");
+paoin=0 ? console.log("El numero es par") : console.log(`El numero ${numero} es impar`);
 
 
