0698103675daf1c18f0761b482bdb402c1e8dd3d
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-10 13:18:07 -0500

    feat: :sparkles: Ejercicio 7
    Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura

--- main.js
+++ main.js
@@ -1,36 +1,27 @@
 /*
-6. Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres.
+7. Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura.
 */
 let datos = {
     nombre:[],
-    sexo:[],
-    nota:[]   
+    precio:[],
+    cantidad:[]   
 }
 let can="si";
 while(can=="si"){
-    let nom = prompt("Ingrese el nombre del estudiante")
-    let sex = (prompt(`${nom} Ingrese su genero Femenino o masculino(F o M)`)).toUpperCase()
-    let nota = Number(prompt(`Ingrese la nota definitiva de ${nom} (0.0 a 5.0)`))
+    let nom = prompt("Ingrese el nombre del articulo")
+    let pre = Number(prompt(`Ingrese el precio del articulo`))
+    let cant = Number(prompt(`Ingrese cuantas  unidades compro`))
     datos.nombre.push(nom)
-    datos.sexo.push(sex)
-    datos.nota.push(nota)
-    can = prompt("Desea ingresar los datos de otro estudiante (si o no): ")
-    
+    datos.precio.push(pre)
+    datos.cantidad.push(cant)
+    can = prompt("Desea ingresar los datos de otro articulo (si o no): ")
 }
-var ch=0;
-var cm =0;
-for(let i=0;i < datos.sexo.length;i++){
-    (datos.sexo[i]=="H")? ch++:cm++
-
-}
+let vlr=0;
 console.table(datos)
-var maximo=Math.max(...datos.nota)
-var minimo= Math.min(...datos.nota)
-console.log(`El estudiante ${datos.nota[datos.nota.indexOf(maximo)]} tiene la mayor nota que es : ${maximo}`)
-console.log(`El estudiante ${datos.nota[datos.nota.indexOf(minimo)]} tiene la mayor nota que es : ${minimo}`)
-console.log(`La cantidad de Mujeres son: ${cm}\nLa cantidad de hombres son ${ch} `)
+for(var i=0;i<datos.precio.length;i++){
+    vlr += (datos.precio[i]*datos.cantidad[i])
+}
 
+console.log(`El valor a pagar de toda la factura es: ${vlr}`)
