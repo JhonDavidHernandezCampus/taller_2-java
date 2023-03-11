98391fe4c9e2d4394a6d6b9583c86c4b35f08d10
JhonDavidHernandezCampus <yonif@gmail.com>
2023-03-10 23:53:31 -0500

    feat: :sparkles: Ejercicio 9
    9. N atletas han pasado a finales en salto triple en los juegos olímpicos femenino de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros.

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 /*
-8. Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo
+9. N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros.
 */
+
 let datos = {
-    medida_del_cuadrado:[],
-    Perimetro_del_cuadrado:[],
-    Medidas_del_triangulo:{altura:[],base:[]},
-    area_del_triangulo:[]
+    nombre:[],
+    msalto:[],
 }
 
 let can="si";
 while(can=="si"){
-    let lado=prompt("Ingrese la medida de los lado del cuadrado")
+    let nom =prompt("Ingrese el nombre del atleta")
+    let sal = Number(prompt(`Ingrese la distancia del salto del atleta ${nom} (en metros (el . separador de desimales))`))
+    datos.nombre.push(nom)
+    datos.msalto.push(sal)
     
-    let base = prompt("Ingrese la base del triangulo")
-    let altura = prompt("Ingrese la altura del triangulo")
-    
-    datos.medida_del_cuadrado.push(lado)
-    datos.Perimetro_del_cuadrado.push(lado*4)
-    datos.Medidas_del_triangulo.altura.push(altura)
-    datos.Medidas_del_triangulo.base.push(base)
-    datos.area_del_triangulo.push((base*altura)/2)
-
-    console.log(`El perimetro del cuadrado es ${lado*4}`)
-    console.log(`El Area del triangulo es: ${(base*altura)/2}`)
-
     can = prompt("Desea ingresar otros datos para ser recalculados(si o no):")
 }
+
+var maximo=Math.max(...datos.msalto)
+var persona =datos.nombre[datos.msalto.indexOf(maximo)]
 console.table(datos)
+console.log(`La persona que se lleva la medalla de ORO es: ${persona}`)
+if(maximo>15.50){
+    console.log(`${persona} ha superado el record y resive 500 millones como bono`) 
+}else{
+    console.log(`${persona} Ha ganado la medalla de oro`)
+}
+
 
 
